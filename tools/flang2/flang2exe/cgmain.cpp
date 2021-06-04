1b15702b02b0112279cf0bcdfdd25cef54f0835f
Bryan Chan <bryan.chan@huawei.com>
2021-06-04 19:13:39 -0400

    [flang1][flang2] Work around warnings with diagnostic pragmas; NFCI
    This patch adds diagnostic pragmas to parts of the code where fixing compile-
    time warnings is non-trivial:
    
    1. -Wcast-qual warnings for type casts to/from hash_key_t and hash_value_t
       due to the const-ness in those typedefs.
    
    2. -Wmissing-field-initializers warnings for structure definitions generated
       with tools such as astutil and fsymutil.

--- tools/flang2/flang2exe/cgmain.cpp
+++ tools/flang2/flang2exe/cgmain.cpp
@@ -8110,6 +8110,8 @@ sincos_argument_valid(int ilix)
   return true;
 }
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic warning "-Wcast-qual"
 INLINE static OPERAND *
 get_last_sincos(int ilix)
 {
@@ -8119,6 +8121,7 @@ get_last_sincos(int ilix)
     return (OPERAND *)data;
   return NULL;
 }
+#pragma GCC diagnostic pop
 
 INLINE static OPERAND *
 gen_llvm_sincos_builtin(int ilix)
